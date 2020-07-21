ffb37f33a1531420a0e92d1099906cac5f696100
JosueD27 <josueamador2711@gmail.com>
2020-07-21 14:45:02 -0600

    Main

--- src/rescateanimal/RescateAnimal.java
+++ src/rescateanimal/RescateAnimal.java
@@ -15,7 +15,8 @@ public class RescateAnimal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MenuPrincipal men = new MenuPrincipal();
+        men.setVisible(true);
     }
     
 }
