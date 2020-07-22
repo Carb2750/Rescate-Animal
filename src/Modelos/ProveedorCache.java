8c86ef64fc500d3bcfb3fe1bdbca736c35dc61b4
gloriapadilla <65581033+gloriapadilla@users.noreply.github.com>
2020-07-21 23:27:08 -0600

    Modelos de datos del proveedor

---
+++ src/Modelos/ProveedorCache.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+import Modelos.Proveedor;
+
+/**
+ *
+ * @author gtorr
+ */
+public class ProveedorCache {
+    private static Proveedor proveedor;
+
+    public static Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public static void setProveedor(Proveedor prov) {
+        ProveedorCache.proveedor = prov;
+    }
+}
